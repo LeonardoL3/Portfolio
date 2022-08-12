33fb23147b1247fc145904ffa41164b9dbc5284f
Leonardo <lelazzati7@gmail.com>
2022-08-11 23:07:10 -0300

    adding github repositories

--- components/ScrollButton.tsx
+++ components/ScrollButton.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import { ArrowDown, ArrowUp } from 'phosphor-react'
 
-const PATHS = ['#home', "#projetos"]
+const PATHS = ['#home', "#projects"]
 
 export const ScrollButton = () => {
 
   const [pathCounter, setPathCounter] = useState(0)
+  const containerRef = useRef(null)
 
   const isCounterEqualToPathsQuantity = pathCounter === PATHS.length - 1
 
@@ -16,11 +17,13 @@ export const ScrollButton = () => {
     } else {
       setPathCounter(prevPathCounter => prevPathCounter + 1)
     }
+
   }
 
+  console.log(containerRef)
 
   return (
-    <div className="w-12 h-12 bg-orange-400 rounded-full hover:-translate-y-2 transition delay-50 fixed bottom-10 right-10">
+    <div ref={containerRef} className="w-12 h-12 bg-orange-400 rounded-full hover:-translate-y-2 transition delay-50 fixed bottom-10 right-10">
       <a href={PATHS[pathCounter]} className="flex justify-center items-center h-full" onClick={handleScroll}>
         {isCounterEqualToPathsQuantity ? <ArrowUp size={32} /> : <ArrowDown size={32} />}
       </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,144 @@
 import React from 'react';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Header from '../components/Header';
 import Image from 'next/image';
 import { ScrollButton } from '../components/ScrollButton';
+import { GithubProjects } from '../components/GithubProjects'
 
+import { LinkedinLogo, GithubLogo, WhatsappLogo, Envelope } from 'phosphor-react'
+import Link from 'next/link';
 
-const Home: NextPage = (props) => {
+type DataProps = {
+
+}
+
+type HomeProps = {
+  data: DataProps
+}
+
+const Home: NextPage = ({ data }: any) => {
+  console.log(data)
   return (
-    <>
-      <div>
-        <div id="home" className="flex h-screen items-center justify-center flex-col">
-          <div className="pb-6 overflow-hidden">
+    <main>
+
+      <section id="home">
+        <div className="flex h-screen items-center justify-center flex-col">
+
+          <div className="rounded-full overflow-hidden border-2 border-solid border-neutral-300 w-[222px] h-[222px]">
             <Image
               src={'https://github.com/Leonardo334.png'}
               width="222px"
               height="222px"
               className="rounded-full border-2 border-solid border-stone-500"
-              //layout="fill"
-              objectFit="cover"
-              style={{ border: '2px solid red' }}
               quality={100}
             />
           </div>
-          <h1 className="text-neutral-500 text-5xl"> Leonardo Lazzaretti </h1>
+          <div className="text-center mx-8">
+            <h1 className="text-neutral-500 text-5xl py-8"> Leonardo Lazzaretti </h1>
+            <p className="text-neutral-300"> I'm a fullstack developer currently working with React and Node. </p>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div id="projetos" className="flex h-screen items-center justify-center flex-col">
-        <div className="pb-6 overflow-hidden">
-          <Image
-            src={'https://github.com/Leonardo334.png'}
-            width="222px"
-            height="222px"
-            className="rounded-full border-2 border-solid border-stone-500"
-            objectFit="cover"
-            style={{ border: '2px solid red' }}
-            quality={100}
-          />
+      <section id="projects">
+        <div className="max-w-[1189px] mx-auto mb-24 px-6">
+          <h2 className="text-xl md:text-3xl  py-12 text-neutral-500"> Some of my projects on Github: </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-12">
+
+            {data?.map((d: any) => {
+              return (
+                <a key={d.id} href={d.html_url} className="h-96 bg-transparent bg hover:scale-105 transition delay-80 outline-4 outline-gray-800">
+                  <div className="h-2/3 overflow-hidden relative rounded-t-lg">
+                    <Image src="https://github.com/Leonardo334.png" layout="fill" className="object-cover" />
+                  </div>
+                  <div className="p-4 h-1/3 text-center border-r border-l border-b border-neutral-300">
+                    <h2 className="text-neutral-300 font-bold text-xl"> {d.name} </h2>
+                    <p className="text-white">
+                      {d.description}
+                    </p>
+                  </div>
+                </a>
+              )
+            })}
+
+
+          </div>
         </div>
-        <h1 className="text-neutral-500 text-5xl"> Projetos </h1>
-      </div>
-    </>
+      </section>
+
+      <footer>
+        <div className="flex justify-center h-24 items-center gap-12">
+          <LinkedinLogo size={24} className="hover:text-orange-400" />
+          <GithubLogo size={24} className="hover:text-orange-400" />
+          <Envelope size={24} className="hover:text-orange-400" />
+          <WhatsappLogo size={24} className="hover:text-orange-400" />
+        </div>
+      </footer>
+
+    </main>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('https://api.github.com/users/Leonardo334/repos')
+  const data = await response.json()
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+
+/*
+
+<div className=" max-w-[1120px] mx-auto flex flex-col justify-center h-screen">
+          <h1 className="text-3xl pb-12"> Some of my projects on github: </h1>
+          <div className="flex gap-12 flex-wrap">
+            <div className=" bg-transparent bg border-2 border-solid rounded-t-3xl hover:scale-105 transition delay-80">
+              <div className="h-2/3 overflow-hidden rounded-t-3xl relative">
+                <Image src="https://github.com/Leonardo334.png" layout="fill" className="object-cover" />
+              </div>
+              <div className="p-4 border-t-2 border-solid border-neutral-300 h-1/3 flex items-center justify-center text-center">
+                <span className="text-primary brightness-150">
+                  um projetinho maneirinho usando next.js e stripe.
+                </span>
+              </div>
+            </div>
+            <div className="w-80 h-80 bg-transparent bg border-2 border-solid border-neutral-300 rounded-t-3xl hover:scale-105 transition delay-80">
+              <div className="h-2/3 overflow-hidden rounded-t-3xl relative">
+                <Image src="https://github.com/Leonardo334.png" layout="fill" className="object-cover" />
+              </div>
+              <div className="p-4 border-t-2 border-solid border-neutral-300 h-1/3 flex items-center justify-center text-center">
+                <span className="text-primary brightness-150">
+                  um projetinho maneirinho usando next.js e stripe.
+                </span>
+              </div>
+            </div>
+            <div className="w-80 h-80 bg-transparent bg border-2 border-solid border-neutral-300 rounded-t-3xl hover:scale-105 transition delay-80">
+              <div className="h-2/3 overflow-hidden rounded-t-3xl relative">
+                <Image src="https://github.com/Leonardo334.png" layout="fill" className="object-cover" />
+              </div>
+              <div className="p-4 border-t-2 border-solid border-neutral-300 h-1/3 flex items-center justify-center text-center">
+                <span className="text-primary brightness-150">
+                  um projetinho maneirinho usando next.js e stripe.
+                </span>
+              </div>
+            </div>
+            <div className="w-80 h-80 bg-transparent bg border-2 border-solid border-neutral-300 rounded-t-3xl hover:scale-105 transition delay-80">
+              <div className="h-2/3 overflow-hidden rounded-t-3xl relative">
+                <Image src="https://github.com/Leonardo334.png" layout="fill" className="object-cover" />
+              </div>
+              <div className="p-4 border-t-2 border-solid border-neutral-300 h-1/3 flex items-center justify-center text-center">
+                <span className="text-primary brightness-150">
+                  um projetinho maneirinho usando next.js e stripe.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        */
