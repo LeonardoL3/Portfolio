6757f8136cf484cc088821e0262915c06a7e13c3
˜LeonardoL3˜ <leonardo.lazzaretti@develcode.com.br>
2022-11-28 02:17:54 -0300

    fix: eslint tab error

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -32,11 +32,11 @@ export function Skills({iconsSize, currentTheme}: IconsGridProps){
 							side='top' 
 							delay={600}
 							className="inline-flex items-center justify-center rounded-md bg-neutral-800 text-white px-4 py-2.5 radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade radix-side-right:animate-slide-left-fade radix-side-top:animate-slide-down-fade"
-							>
-						<div className="keen-slider__slide flex justify-center"> 
-							<Icon variant={skill} size={iconsSize} color={currentTheme === 'dark' ? 'white' : 'black'} className="dt" />
-						</div>
-							</Tooltip>
+						>
+							<div className="keen-slider__slide flex justify-center"> 
+								<Icon variant={skill} size={iconsSize} color={currentTheme === 'dark' ? 'white' : 'black'} className="dt" />
+							</div>
+						</Tooltip>
 					)
 				})}
 			</div>
