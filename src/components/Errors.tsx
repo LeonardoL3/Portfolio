cb93c276f26b6f9e0bb65c808f9aa644f026eea7
˜LeonardoL3˜ <leonardo.lazzaretti@develcode.com.br>
2022-12-12 08:44:28 -0300

    fix: 404 page dark theme

--- src/components/Errors.tsx
+++ src/components/Errors.tsx
@@ -11,12 +11,12 @@ export function Errors({statusCode, message}: ErrorsProps){
 	return (
 		<section>
 			<div className='flex flex-col gap-12 justify-center items-center h-screen'>
-				<div className="text-dark-300 dark:text-light-100">
+				<div className="text-dark-900 dark:text-light-100">
 					<span>{statusCode}: &nbsp;</span>
 					<span>{message}</span>
 				</div>
 				<Link href='/'>
-					<Button aria-label="back to home" className="border-2 p-4 rounded-md flex items-center gap-2 text-dark-100 dark:text-light-100 dark:border-light-100 border-dark-900 hover:scale-110 transition-all">
+					<Button aria-label="back to home" className="border-2 p-4 rounded-md flex items-center gap-2 dark:text-light-100 text-dark-900 dark:border-light-100 border-dark-900 hover:scale-110 transition-all">
                     Back to home <AiFillHome size={18} className='inline' /> 
 					</Button> 
 				</Link>
