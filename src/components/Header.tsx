6dff0859780b5e1d0f5785f4e539646adeaf59f9
LeonardoL3 <leonardolazzaretti3@gmail.com>
2026-03-27 21:36:03 -0300

    style: remove header bottom margin on larger screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Languages } from './Languages'
 export function Header(){
 
 	return (
-		<header id="header" className="w-full flex items-center p-4 mb-4 gap-4 justify-end">
+		<header id="header" className="w-full flex items-center p-4 gap-4 justify-end sm:mb-0 mb-4">
 			<ThemeToast />
 			<Languages />
 		</header>
